17bfe5d0cf44fc0c2f731a73609634623a088c59
dkrawciw <dkrawciw@mines.edu>
2022-02-26 16:44:45 -0700

    Added directory for html files and rendering html

--- serverSide.py
+++ serverSide.py
@@ -1,11 +1,12 @@
 import json
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-app.route('/')
+@app.route('/')
 def index():
-    return 'Test'
+    return render_template("index.html")
 
 app.run(host="0.0.0.0", port=8080)
 
