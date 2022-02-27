1e7bd026ebf7ceaefa5e15f73f74214b76562521
dkrawciw <dkrawciw@mines.edu>
2022-02-27 15:36:49 -0700

    Added nodejs server for json files

--- serverSide/app.js
+++ serverSide/app.js
@@ -1,1 +1,13 @@
-console.log("Yooo");
+const express = require("express"),
+      app = express();
+
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile("index.html");
+});
+
+app.listen(8080, () => {
+    console.log("Server Up 'n Running!");
+});
