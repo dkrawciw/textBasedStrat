ca305e6ca7d4bc89bf695040a042bc82c2ab6b72
Ben <benjamin.pacheco@s.stemk12.org>
2022-03-12 22:47:10 -0700

    Created button post request for an esp32s2 board

---
+++ microcontroller/buttonPostRequestESP32S2/buttonPostRequestESP32S2.ino
@@ -0,0 +1,57 @@
+#include <Arduino.h>
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+
+#include <HTTPClient.h>
+
+#define USE_SERIAL Serial
+
+const int buttonPin = 10;
+int buttonState = 0;
+
+WiFiMulti wifiMulti;
+void setup() {
+
+  // Start serial connection
+  Serial.begin(115200);
+  
+  // Wait for boot
+  for(uint8_t t = 4; t > 0; t--) {
+        Serial.printf("[SETUP] WAIT %d...\n", t);
+        Serial.flush();
+        delay(1000);
+  }
+    
+  // Wifi Information
+  wifiMulti.addAP("ServerMGMT", "foosballchamp2");
+
+  // Setup button pin
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+   // Read button state
+   buttonState = digitalRead(buttonPin);
+
+   
+   // wait for WiFi connection
+   if((wifiMulti.run() == WL_CONNECTED)) {
+       WiFiClient client;
+       HTTPClient http;
+
+       // Server ip
+       http.begin(client, "http://192.168.1.13:8080/addMoneyTest");
+
+       // Check if button is pushed
+       if (buttonState == HIGH){
+           // Make post request
+           http.POST("a");
+
+           // Alert user request made
+           Serial.println("Post request made");
+           delay(500);
+       }
+
+   }
+}
