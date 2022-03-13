d0818013cd63f43f481d0265180c9193c15cc6de
dkrawciw <dkrawciw@mines.edu>
2022-03-12 23:17:10 -0700

    Changed url and added node_modules/ to the .gitignore

--- clientSide/101CreateProject.py
+++ clientSide/101CreateProject.py
@@ -1,8 +1,8 @@
 import requests
 
-x = requests.get("http://127.0.0.1:8080/guestList.txt")
+x = requests.get("http://bkp515.com:8080/guestList.txt")
 print(f"Here is my current guest:\n{x.text}")
 
 
 name = input("Input your name into the guest list: ")
-r = requests.post("http://127.0.0.1:8080/addGuestList", data={"name": name})
+r = requests.post("http://bkp515.com:8080/addGuestList", data={"name": name})
